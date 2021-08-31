23ed2a9a58f9956ab2a0fde723916b6f1ab4d1e5
chrystalme <afam_ifeanyi@live.com>
2021-08-31 15:11:21 +0300

    Add measurement controller and concerns

--- app/controllers/measurements_controller.rb
+++ app/controllers/measurements_controller.rb
@@ -1,2 +1,47 @@
 class MeasurementsController < ApplicationController
+  before_action :set_measure
+  before_action :set_measure_measurement, only %i[show update destroy]
+
+  #GET /measures/:measure_id/measurements
+  def index
+    json_response(@measure.measurements)
+  end
+  
+  #GET /measures/:measure_id/measurements/:id
+  def show
+    json_response(@measurement)
+  end
+  
+  #POST /measures/:measure_id/measurements
+  def create
+    @measure.measurements.create!(measurement_params)
+    json_response(@measure, :created)
+  end
+  
+  #PUT /measures/:measure_id/measurements/:id
+  def update
+    @measurement.update(measurement_params)
+    head :no_content
+  end
+  
+  #DELETE /measures/:measure_id/measurements/:id
+  def destroy
+    @measurement.destroy
+    head :no_content
+  end
+
+  private
+  def measurement_params
+    params.permit(:value, :date)
+  end
+
+  def set_measure
+    @measure = Measure.find(params[:measure_id])
+  end
+
+  def set_measure_measurement
+    @measurement = @measure.measurements.find_by(id: :params[:id]) if @measure
+  end
+
+
 end
--- app/models/measure.rb
+++ app/models/measure.rb
@@ -1,5 +1,5 @@
 class Measure < ApplicationRecord
-  has_many measurements, dependent: :destroy
+  has_many :measurements, dependent: :destroy
 
   validates_presence_of :title, :image, :description
 end
