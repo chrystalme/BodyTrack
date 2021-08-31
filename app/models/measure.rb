74332809315bf00fb24a6147eade7c4763daef37
chrystalme <afam_ifeanyi@live.com>
2021-08-31 14:32:36 +0300

    Add routes for measures and measurements

--- app/models/measure.rb
+++ app/models/measure.rb
@@ -1,2 +1,5 @@
 class Measure < ApplicationRecord
+  has_many measurements, dependent: :destroy
+
+  validates_presence_of :title, :image, :description
 end
--- app/models/measurement.rb
+++ app/models/measurement.rb
@@ -1,3 +1,4 @@
 class Measurement < ApplicationRecord
   belongs_to :measure
+  validates_presence_of :value, :date
 end
