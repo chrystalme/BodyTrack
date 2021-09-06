d2620903f16f15a69d8801a177f4862ea19a44ea
chrystalme <afam_ifeanyi@live.com>
2021-09-06 21:05:34 +0300

    Add fast_jsonapi

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,6 +2,9 @@
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 #
 # Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+
+Measure.create!(title: 'Height', description: 'Measure of head to feet', image: '/public/images/31010672.jpg')
+Measure.create!(title: 'Weight', description: 'Mass of the baby without much clothes', image: '/public/images/31010672.jpg')
+Measure.create!(title: 'Feet', description: 'Length of feet', image: '/public/images/31010672.jpg')
+Measure.create!(title: 'Hand', description: 'Length of hand from the finger to the shoulder', image: '/public/images/31010672.jpg')
+Measure.create!(title: 'Leg', description: 'Length of leg from foot to waist ', image: '/public/images/31010672.jpg')
