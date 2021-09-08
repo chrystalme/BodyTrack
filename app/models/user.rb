78bf1d03fe62e73d9eeb17e3451ae45ef110d906
chrystalme <afam_ifeanyi@live.com>
2021-09-08 10:50:57 +0300

    Update JsonWebToken file

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,5 @@ class User < ApplicationRecord
   has_many :measures
 
   validates_presence_of :name, :email, :password_digest
+  validates_uniqueness_of :email
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,11 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      resources :measure do
-        resources :measurement
-      end
+      resources :measure
+      resources :measurement
+
     end
   end
-
   post 'auth/login', to: 'authentication#authenticate'
   post 'signup', to: 'users#create'
 end
