f11a9f38b77e454c3d340915961c8e6b15264f51
chrystalme <afam_ifeanyi@live.com>
2021-09-14 11:00:33 +0300

    Update Migration files

--- db/migrate/20210913185707_add_column_unit_to_measure.rb
+++
@@ -1,4 +0,0 @@
-class AddColumnUnitToMeasure < ActiveRecord::Migration[6.1]
-  def change
-  end
-end
---
+++ db/migrate/20210913185707_add_column_unit_to_measure_empty.rb
@@ -0,0 +1,4 @@
+class AddColumnUnitToMeasureEmpty < ActiveRecord::Migration[6.1]
+  def change
+  end
+end
